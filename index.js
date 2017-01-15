834c7efc03dc7b250a65a233e0c0ce331b2fef49
reSHARMA <reshabhsh@gmail.com>
2017-01-15 22:50:09 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,12 @@
+
+var express = require ('express');
+var app = express();
+var server = require ('http').createServer(app);
+var io = require('socket.io')(server);
+app.get('/',function(req,res){
+res.send("PI conf page running ...")
+});
+var ioClient = require ('socket.io-client');
+var socketClient = ioClient.connect(https://relock.herokuapp.com/);
+var port = process.env.PORT || 3001;
+server.listen(port);
