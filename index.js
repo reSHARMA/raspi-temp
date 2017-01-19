46ff2b8e60a6ae7f14550dba32b5200d22e972fa
reSHARMA <reshabhsh@gmail.com>
2017-01-19 05:18:51 +0000

    Update index.js

--- index.js
+++ index.js
@@ -36,8 +36,9 @@ socketClient.on('connect',function(){
    
    var interval = setInterval(function() {
  console.log("pi connected");
- socketClient.emit('remote','connected successfully'); 
-}, 5000);
+ socketClient.emit('remote','connected successfully');
+ socketClient.emit('stat',status());
+}, 10000);
   
 });
    socketClient.on('pi',function(data){
